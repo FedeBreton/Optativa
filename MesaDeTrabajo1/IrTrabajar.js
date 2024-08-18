c60fc1e9c3558dad342cb7c96ebb229761b167d5
Fede Breton <fede8005@hotmail.com>
2024-08-18 20:22:24 -0300

    first commit

---
+++ MesaDeTrabajo1/IrTrabajar.js
@@ -0,0 +1,20 @@
+let lluvia = true;
+console.log(lluvia);
+
+let temperatura = 15;
+console.log(temperatura);
+
+let hora_actual = 7;
+console.log(hora_actual);
+
+let encender_moto = true;
+console.log(encender_moto);
+
+let fichar_hora_entrada = 8;
+console.log(fichar_hora_entrada);
+
+let fichar_hora_salida = 12;
+console.log(fichar_hora_salida);
+
+let regresar_a_casa = true;
+console.log(regresar_a_casa);
